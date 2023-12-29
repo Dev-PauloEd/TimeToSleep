f874ad0b1b421c6ef297b992d2de2828e3d5ba2f
Paulo Eduardo <pe831365@gmail.com>
2023-12-28 21:52:10 -0300

    Just modifying a bit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 import time
 
 tempo = datetime.datetime.now() # Armazena o horário atual
-while tempo.hour <= 21 or tempo.minute < 30: # Verifica se são 21:30h
+while tempo.hour <= 21 or tempo.minute <= 30: # Verifica se são 21:30h
     print('verificação')
     time.sleep(300) # Se for falso, espera 5m
     tempo = datetime.datetime.now() # Volta pra linha 5
